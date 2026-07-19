396da728d90a84aad5141c6f9206338ef645e4d8
Vipin George Abraham <65977998+vga-t@users.noreply.github.com>
2026-07-19 19:42:35 +0200

    Add: is-palindrome - submission-13

---
+++ Data Structures & Algorithms/is-palindrome/submission-13.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s)-1
+        while left < right:
+            is_left_character_alpha = s[left].isalnum()
+            is_right_character_alpha = s[right].isalnum()
+            if not is_left_character_alpha:
+                left+=1
+            if not is_right_character_alpha:
+                right-=1
+
+
+            if is_left_character_alpha and is_right_character_alpha:
+                if s[left].lower() != s[right].lower():
+                    return False
+                else:
+                    left+=1
+                    right-=1
+
+        return True
+
+            
+        
